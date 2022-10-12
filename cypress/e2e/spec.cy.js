6d0d5ac9c62bde584da833f292b5ad43eabbe8ab
Stokes Player <stokes.player@gmail.com>
2022-10-12 08:02:18 -0400

    reproduction for issue 24164

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,19 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
+  it.skip('works', () => {
     cy.visit('https://example.cypress.io')
+  })
+
+  it('does not retry', () => {
+    cy.intercept('/').as('page')
+    cy.visit('https://example.cypress.io')
+    cy.wait('@page').should('have.property', 'foo')
+  })
+
+
+  it('is slow', () => {
+    cy.intercept('/').as('page')
+    cy.visit('https://example.cypress.io')
+    cy.wait('@page').its('response.statusCode').should('eq', 500)
   })
 })
